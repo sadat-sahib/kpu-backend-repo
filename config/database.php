7d50d7213be823f9b6ff593704a14280693bad4b
sayed-musa <ahmadeyan22@gmail.com>
2025-12-08 16:20:44 +0430

    fix ssl

--- config/database.php
+++ config/database.php
@@ -59,6 +59,8 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::ATTR_EMULATE_PREPARES => true,
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
             ]) : [],
         ],
 
