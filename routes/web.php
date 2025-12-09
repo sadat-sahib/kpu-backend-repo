9fe6dc388b32d1ff81873df2b6de2c7f7916d86b
sayed-musa <ahmadeyan22@gmail.com>
2025-12-09 13:48:06 +0430

    Add database connection test route
    Add a route to test database connection and handle exceptions.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/test-db', function() {
+    try {
+        \DB::connection()->getPdo();
+        return 'DB connected!';
+    } catch (\Exception $e) {
+        return $e->getMessage();
+    }
+});
 Route::get('/', function () {
     return view('welcome');
 });
