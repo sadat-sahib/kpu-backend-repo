9a10fc9c98c565b1753b99ce474fdf5955e4c6e8
sayed-musa <ahmadeyan22@gmail.com>
2025-12-09 13:47:13 +0430

    Update database.php

--- config/database.php
+++ config/database.php
@@ -58,9 +58,8 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-                PDO::ATTR_EMULATE_PREPARES => true,
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+                PDO::ATTR_EMULATE_PREPARES => true,
             ]) : [],
         ],
 
